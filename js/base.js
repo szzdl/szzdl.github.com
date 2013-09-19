9fbccea205e0138c8083c74c535e83cfcbd4447b
ives <ivesxiao@msn.com>
2013-09-19 09:47:14 +0800

    test

--- js/base.js
+++ js/base.js
@@ -1,16 +1,24 @@
 var ZDL = function(){
-	this.base_path = "szzdl.github.com";
+	this.base_path = "http://szzdl.github.com";
 	this.resources = ['/jquery/css/jquery.mobile-1.3.2.min.css',
 					 '/jquery/js/jquery-1.10.1.min.js',
-					 'jquery/js/mobile-1.3.2.min.js'
+					 '/jquery/js/jquery.mobile-1.3.2.min.js'
 					];
-}
+};
+
 
 ZDL.prototype.init = function(){
+	var head = document.getElementsByTagName('head')[0];
+	var ele = document.createElement('meta');
+	ele.setAttribute('charset', 'UTF-8');
+	head.appendChild(ele);
+	ele = document.createElement('meta');
+	ele.setAttribute('name', 'viewport');
+	ele.setAttribute('content', 'width=device-width, initial-scale=1');
+	head.appendChild(ele);
 	for(var key in this.resources){
 		var path = this.base_path + this.resources[key];
 		var end = path.substr(path.length - 3, 3);
-		var ele;
 		if(end == '.js'){
 			ele = document.createElement('script');
 			ele.setAttribute('type', 'text/javascript');
@@ -22,5 +30,9 @@ ZDL.prototype.init = function(){
 			ele.setAttribute('type', 'text/css');
 			ele.setAttribute('href', path);
 		}
+		head.appendChild(ele);
 	}
 }
+
+var zdl = new ZDL();
+zdl.init();
